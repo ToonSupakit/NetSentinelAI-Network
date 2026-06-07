effe0e80c2e0f1c497a6076c1960b3d6e8eda772
supakit-1 <6631501109@lamduan.mfu.ac.th>
2026-06-07 23:53:15 +0700

    chore: cleanup repository by removing temporary docs and untracking scratch folder

--- scratch/debug_snmp.py
+++
@@ -1,8 +0,0 @@
-from app.snmp_helper import get_snmp_interfaces
-
-result = get_snmp_interfaces("10.10.100.1", "public")
-for r in result:
-    if r["ip"] != "unassigned":
-        print(
-            f"  {r['intf']:20s} rel={r['reliability']:3d} tx={r['network_load']:3d} rx={r['rxload']:3d} err={r['input_errors']}"
-        )
